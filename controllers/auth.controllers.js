774f763a934755c424d4bf60c72ce391810e1d6b
monibix <monibixv2@gmail.com>
2021-03-15 13:47:18 +0100

    fixed editUser controller

--- controllers/auth.controllers.js
+++ controllers/auth.controllers.js
@@ -8,38 +8,27 @@ const {
 
 exports.signup = async (req, res) => {
   try {
-    console.log("console")
     const { password, email } = req.body;
     const hasMissingCredentials = !password || !email;
     if (hasMissingCredentials) {
       return res.status(400).json({ message: "missing credentials" });
     }
-    console.log("reqbody", req.body)
     if (!hasCorrectPasswordFormat(password)) {
       console.log("password", password)
       return res.status(400).json({ message: "incorrect password format" });
     }
-    console.log("reqbody", req.body)
     const user = await User.findOne({ email });
-
-    console.log("user", user)
-
     if (user) {
-      
       return res.status(400).json({ message: "user alredy exists" });
     }
-
     const saltRounds = 10;
     const salt = await bcrypt.genSalt(saltRounds);
     const hashedPassword = await bcrypt.hash(password, salt);
     const newUser = await User.create({ email, hashedPassword });
-    console.log("reqsession", req.session)
     console.log("newuser", newUser)
     req.session.userId = newUser._id;
-    console.log("console linia 37", req.session)
     return res.status(200).json({ user: newUser.email, id: newUser._id });
   } catch (e) {
-    console.log("e", e)
     if (isMongooseErrorValidation(e)) {
       return res.status(400).json({ message: "incorrect email format" });
     }
@@ -57,11 +46,9 @@ exports.login = async (req, res) => {
     if (hasMissingCredentials) {
       return res.status(400).json({ message: "missing credentials" });
     }
-
     if (!hasCorrectPasswordFormat(password)) {
       return res.status(400).json({ message: "incorrect password" });
     }
-
     const user = await User.findOne({ email });
     if (!user) {
       return res.status(400).json({ message: "user does not exist" });
@@ -73,9 +60,7 @@ exports.login = async (req, res) => {
     if (!hasCorrectPassword) {
       return res.status(401).json({ message: "unauthorize" });
     }
-
     req.session.userId = user._id;
-
     return res.status(200).json({ user: user.email, id: user._id });
   } catch (e) {
     if (isMongooseErrorValidation(e)) {
@@ -92,8 +77,13 @@ exports.logout = async (req, res) => {
 
 exports.getUser = async (req, res) => {
   const { userId } = req.session;
-  const { email, _id } = await User.findOne(userId);
-  res.status(200).json({ id: _id, email });
+  // const { email, _id } = await User.findOne(userId);
+  // res.status(200).json({ id: _id, email });
+  console.log("userId", userId);
+  const { hashedPassword, ...user } = await User.findById({
+    _id: userId,
+  }).lean();
+  res.status(200).json(user);
 };
 
 //no funciona en postman
@@ -103,7 +93,8 @@ exports.editUser = async(req, res) => {
     console.log("reqsession", req.session)
     const userInfo = req.body; //undefined
     console.log("reqbody", req.body)
-    const updatedUser = await User.findByIdAndUpdate(userId, userInfo)
+    const updatedUser = await User.findByIdAndUpdate({_id: userId}, userInfo) //userId
+    console.log("updatedUser", updatedUser)
     res.status(200).json(updatedUser)
   }
   catch(error){
