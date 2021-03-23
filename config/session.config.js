08261635c83ebc16b928a6f9f25c0e7694b8419f
monibix <monibixv2@gmail.com>
2021-03-23 22:13:39 +0100

    modificado archivo session.config con codigo tomas

--- config/session.config.js
+++ config/session.config.js
@@ -1,16 +1,39 @@
+// require("dotenv").config();
+// const session = require("express-session");
+// const MongoStore = require("connect-mongo");
+
+// const { SESSION_SECRET, MONGODB_URI } = process.env;
+// console.log(MONGODB_URI);
+// module.exports = (app) => {
+//   app.use(
+//     session({
+//       secret: SESSION_SECRET,
+//       resave: true,
+//       saveUninitialized: true,
+//       cookie: { maxAge: 60000000, secure: false },
+//       store: MongoStore.create({
+//         mongoUrl: MONGODB_URI,
+//         ttl: 60 * 60 * 24,
+//       }),
+//     })
+//   );
+// };
+
 require("dotenv").config();
 const session = require("express-session");
 const MongoStore = require("connect-mongo");
-
-const { SESSION_SECRET, MONGODB_URI } = process.env;
-console.log(MONGODB_URI);
+const { SESSION_SECRET, MONGODB_URI, NODE_ENV } = process.env;
+const isProduction = NODE_ENV === "production";
 module.exports = (app) => {
+  if (isProduction) {
+    app.set("trust proxy", 1);
+  }
   app.use(
     session({
       secret: SESSION_SECRET,
-      resave: true,
+      resave: false,
       saveUninitialized: true,
-      cookie: { maxAge: 60000000, secure: false },
+      cookie: { maxAge: 6000000, secure: isProduction ? true : false },
       store: MongoStore.create({
         mongoUrl: MONGODB_URI,
         ttl: 60 * 60 * 24,
--- middlewares/withAuth.js
+++ middlewares/withAuth.js
@@ -1,5 +1,5 @@
 exports.withAuth = (req, res, next) => {
-  console.log("req.session", req.sessiongit)
+  console.log("req.session", req.session)
   if (!req.session.userId) {
     return res.status(401).json({ message: "unauthorized" });
   }
