cec4a47941af49dab0f76d48ac2f753768c6641c
monibix <monibixv2@gmail.com>
2021-03-23 22:39:00 +0100

    cambiado orden session-middlewares en app.js

--- app.js
+++ app.js
@@ -3,8 +3,8 @@ const express = require("express");
 const app = express();
 
 require("./config/db.config")();
-require("./config/middleware.config")(app);
 require("./config/session.config")(app);
+require("./config/middleware.config")(app);
 require("./config/cloudinary-setup.config")
 
 const authRoutes = require("./routes/auth.routes");
