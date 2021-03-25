94fef4f146065d04388a34fa82d52c90e1831ab4
monibix <monibixv2@gmail.com>
2021-03-25 07:17:21 +0100

    update session.config

--- config/session.config.js
+++ config/session.config.js
@@ -19,24 +19,47 @@
 //   );
 // };
 
+// require("dotenv").config();
+// const session = require("express-session");
+// const MongoStore = require("connect-mongo");
+// const { SESSION_SECRET, MONGODB_URI, NODE_ENV } = process.env;
+// const isProduction = NODE_ENV === "production";
+// module.exports = (app) => {
+//   if (isProduction) {
+//     app.set("trust proxy", 1);
+//   }
+//   app.use(
+//     session({
+//       secret: SESSION_SECRET,
+//       resave: false,
+//       saveUninitialized: true,
+//       cookie: { maxAge: 6000000, secure: isProduction ? true : false },
+//       store: MongoStore.create({
+//         mongoUrl: MONGODB_URI,
+//         ttl: 60 * 60 * 24,
+//       }),
+//     })
+//   );
+// };
+
+
 require("dotenv").config();
 const session = require("express-session");
 const MongoStore = require("connect-mongo");
 const { SESSION_SECRET, MONGODB_URI, NODE_ENV } = process.env;
 const isProduction = NODE_ENV === "production";
+const sameSite = isProduction ? "none" : "lax";
 module.exports = (app) => {
-  if (isProduction) {
-    app.set("trust proxy", 1);
-  }
+  app.set("trust proxy", 1);
   app.use(
     session({
       secret: SESSION_SECRET,
-      resave: false,
-      saveUninitialized: true,
-      cookie: { maxAge: 6000000, secure: isProduction ? true : false },
+      resave: true,
+      saveUninitialized: false,
+      cookie: { maxAge: 360000 * 24 * 14, secure: isProduction, sameSite },
       store: MongoStore.create({
         mongoUrl: MONGODB_URI,
-        ttl: 60 * 60 * 24,
+        ttl: 360000 * 24 * 14,
       }),
     })
   );
